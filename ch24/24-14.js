344140814a0694d82751644c0b3c1b078e25be30
nataekoon <smathj@naver.com>
2022-05-18 23:45:28 +0900

    0518

---
+++ ch24/24-14.js
@@ -0,0 +1,26 @@
+function makeCounter(predicate) {
+  let counter = 0;
+
+  return function () {
+    counter = predicate(counter);
+    return counter;
+  };
+}
+
+function increase(n) {
+  return ++n;
+}
+
+// 보조 함수
+function decrease(n) {
+  return --n;
+}
+
+// 함수로 함수를 생성한다
+const increaser = makeCounter(increase);
+console.log(increaser());
+console.log(increaser());
+
+const decreaser = makeCounter(decrease);
+console.log(decreaser());
+console.log(decreaser());
